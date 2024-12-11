329f1b0e50a471dbb57285fa8768ce0cb3a5717c
SamvitValluri <samvit.valluri@gmail.com>
2024-12-11 15:02:48 -0500

    links

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -13,9 +13,8 @@ const hero: {
 	description:
 		'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
 	links: [
-		{ label: 'GitHub', href: 'https://github.com', icon: 'i-carbon-logo-github' },
-		{ label: 'LinkedIn', href: 'https://linkedin.com', icon: 'i-carbon-logo-linkedin' },
-		{ label: 'Twitter', href: 'https://twitter.com', icon: 'i-carbon-logo-twitter' },
+		{ label: 'GitHub', href: 'https://github.com/SamvitValluri', icon: 'i-carbon-logo-github' },
+		{ label: 'LinkedIn', href: 'https://www.linkedin.com/in/samvit-valluri/', icon: 'i-carbon-logo-linkedin' },
 		{ label: 'Email', href: 'mailto:samvit.valluri@gmail.com', icon: 'i-carbon-at' }
 	]
 };
