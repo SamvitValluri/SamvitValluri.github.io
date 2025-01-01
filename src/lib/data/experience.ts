ac4c1bbb812525981c127380a47d82ed4e5313ef
SamvitValluri <samvit.valluri@gmail.com>
2024-12-31 21:12:11 -0500

    .

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -1,26 +1,54 @@
 import Assets from './assets';
 import { getSkills } from './skills';
-import { ContractType, type Experience } from './types';
+import { ContractType, type Project } from './types';
 
 
 const title = 'Projects';
 
-const items: Array<Experience> = [
+const items: Array<Project> = [
+
 	{
-		slug: 'ME463',
-		company: '',
-		description: '',
-		contract: ContractType.Freelance,
-		type: '',
-		location: '',
-		period: { from: new Date(2022, 0, 1), to: new Date() },
-		skills: getSkills('css', 'html', 'js'),
-		name: 'Automated Table Tennis Paddle Manufacturer',
-		color: 'green',
-		links: [],
-		logo: Assets.Unknown,
-		shortDescription: ''
-	}
+		slug: 'slick-portfolio-svelte',
+		color: '#ff3e00',
+		description:
+			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
+		shortDescription:
+			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
+		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }],
+		logo: Assets.Svelte,
+		name: 'Slick Portfolio',
+		period: {
+			from: new Date()
+		},
+		skills: getSkills('svelte', 'ts', 'tailwind', 'sass'),
+		type: 'Website Template',
+		screenshots: [
+			{
+				label: 'screen 1',
+				src: 'https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '2',
+				src: 'https://images.unsplash.com/photo-1516116216624-53e697fedbea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '3',
+				src: 'https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '4',
+				src: 'https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '5',
+				src: 'https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			},
+			{
+				label: '6',
+				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
+			}
+		]
+	},
 ];
 
 const ExperienceData = { title, items };
